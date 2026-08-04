4281eab0dd08cb1de9698ecaa9feff9f7b3759e4
Knysh <knysh@Knysh-MacBook-Pro.local>
2026-08-05 01:19:42 +0300

    add test_all_brands_have_correct_structure

--- tests/test_brands.py
+++ tests/test_brands.py
@@ -7,4 +7,18 @@ def test_get_brands_list(products_client):
     data = response.json()
     assert 'brands' in data
     assert len(data['brands']) > 0
-    
+
+def check_brand_structure(brand):
+    required_fields = ['id', 'brand']
+
+    for field in required_fields:
+        assert field in brand, f"Поле '{field}' отсутствует в бренде: {brand}"
+
+    assert isinstance(brand['id'], int)
+    assert isinstance(brand['brand'], str)
+
+def test_all_brands_have_correct_structure(products_client):
+    response = products_client.get_brands_list()
+    brands = response.json()['brands']
+    for brand in brands():
+        check_brand_structure(brand)
